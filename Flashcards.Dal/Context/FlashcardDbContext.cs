3c940e558f160e9c804f3eba86fb3d09f1d4324f
Katarzyna-Stulgis <kasia2011r@gmail.com>
2022-08-17 16:28:35 +0200

    Add DbContext and first migration

---
+++ Flashcards.Dal/Context/FlashcardDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Flashcards.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flashcards.Dal.Context
+{
+    public class FlashcardDbContext : DbContext
+    {
+        public FlashcardDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Folder> Folders { get; set; }
+        public DbSet<Deck> Decks { get; set; }
+        public DbSet<Flashcard> Flashcards { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        protected override async void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Flashcard>()
+            .HasMany(p => p.Users)
+            .WithMany(p => p.Flashcards)
+            .UsingEntity<FlashcardLevel>(
+                j => j
+                    .HasOne(pt => pt.User)
+                    .WithMany(t => t.FlashcardLevels)
+                    .HasForeignKey(pt => pt.UserId),
+                j => j
+                    .HasOne(pt => pt.Flashcard)
+                    .WithMany(p => p.FlashcardLevels)
+                    .HasForeignKey(pt => pt.FlashcardId),
+                j =>
+                {
+                    j.HasKey(t => new { t.UserId, t.FlashcardId });
+                });
+
+            modelBuilder.Entity<Flashcard>()
+                .HasOne(e => e.Deck)
+                .WithMany(e => e.Flashcards).OnDelete(DeleteBehavior.ClientCascade);
+        }
+    }
+}
