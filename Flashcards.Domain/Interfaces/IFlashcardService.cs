b1293db279f4081bd32db54b04de279c353d8890
Katarzyna-Stulgis <kasia2011r@gmail.com>
2022-08-22 13:42:30 +0200

    Add interfaces

---
+++ Flashcards.Domain/Interfaces/IFlashcardService.cs
@@ -0,0 +1,12 @@
+﻿namespace Flashcards.Domain.Interfaces
+{
+    public interface IFlashcardService<T> where T : class
+    {
+        abstract Task<IEnumerable<T>> GetAllAsync();
+        abstract Task<T> GetAsync(int id);
+        abstract Task<T> AddAsync(T entity);
+        abstract Task<T> UpdateAsync(T entity);
+        abstract Task<T> DeleteAsync(int id);
+        abstract bool ExistsAsync(int id);
+    }
+}
