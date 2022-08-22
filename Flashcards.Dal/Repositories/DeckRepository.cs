937cee5ddde23173419ea5f45c1f84ae43345e39
Katarzyna-Stulgis <kasia2011r@gmail.com>
2022-08-22 14:58:05 +0200

    Fix: DeckRepository

--- Flashcards.Dal/Repositories/DeckRepository.cs
+++ Flashcards.Dal/Repositories/DeckRepository.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Flashcards.Dal.Repositories
 {
-    internal class DeckRepository : EFCoreRepository<Deck, DbContext>
+    public class DeckRepository : EFCoreRepository<Deck, DbContext>
     {
         public DeckRepository(DbContext dbContext) : base(dbContext) { }
     }
