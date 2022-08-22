29a69c18c4795e3338b34185b4bc343419c3878d
Katarzyna-Stulgis <kasia2011r@gmail.com>
2022-08-22 14:59:17 +0200

    Add Services and Repositories Injection

--- Flashcards.Api/Program.cs
+++ Flashcards.Api/Program.cs
@@ -1,4 +1,8 @@
 using Flashcards.Dal.Context;
+using Flashcards.Domain.Interfaces;
+using Flashcards.Domain.Models.Entities;
+using Flashcards.Service.DataServices;
+using Flashcards.Dal.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +16,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DbContext
+builder.Services.AddTransient<DbContext, FlashcardDbContext>();
+
+// Services Injection
+builder.Services.AddScoped<IFlashcardService<Deck>, DeckService>();
+
+
+// Repositories Injection
+builder.Services.AddScoped<IFlashcardsRepository<Deck>, DeckRepository>();
 
 var app = builder.Build();
 
